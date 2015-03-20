6a488e2d9e334e40d655711f7d50307d334d327a
wiiiky <wiiiky@outlook.com>
2015-03-20 16:53:17 +0800

    添加配置文件读取的函数

--- jmod/jmod.c
+++ jmod/jmod.c
@@ -16,7 +16,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
  */
 #include "jmod.h"
-#include <jlib/jlib.h>
 #include <jio/jio.h>
 #include <stdio.h>
 #include <dlfcn.h>
