653ecb2f77e4c10546c66fb659f54c47ab98a35e
wiiiky <wiiiky@outlook.com>
2015-04-30 20:36:19 +0800

    mainloop TODO

---
+++ jlib/jmainloop.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2015  Wiky L
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with main.c; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+#include "jmainloop.h"
+#include "jthread.h"
+#include "jslist.h"
+#include "jlist.h"
+#include "jhashtable.h"
+#include "jarray.h"
+
+
+struct _JSourceFuncs{
+    jboolean (*prepare)(JSource *source, jint *timeout);
+    jboolean (*check)(JSource *source);
+    jboolean (*dispatch)(JSource *source,JSourceFunc callback,
+                         jpointer user_data);
+    void (*finalize)(JSource *source);
+};
+
+
+struct _JSourceCallbackFuncs{
+    void (*ref) (jpointer cb_data);
+    void (*unref) (jpointer cb_data);
+    void (*get) (jpointer cb_data, JSource *source,
+                 JSourceFunc *func,
+                 jpointer *data);
+};
+
+struct _JSource{
+    JSourceCallbackFuncs *callback_funcs;
+    jpointer callback_data;
+    
+};
+
+struct _JMainContext{
+    JMutex mutex;
+    JCond cond;
+    JThread *owner;
+    juint owner_count;
+    JSList *waiters;
+
+    jint ref;
+    JHashTable *sources;    /* juint -> JSource */
+
+    JPtrArray *pending_despatches;
+    jint timeout;           /* timeout for current iteration */
+
+    juint next_id;
+    JList *source_lists;
+    jint in_check_or_prepare;
+    /* TODO */
+};
---
+++ jlib/jmainloop.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2015  Wiky L
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with main.c; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+#ifndef __JLIB_MAINLOOP_H__
+#define __JLIB_MAINLOOP_H__
+#include "jtypes.h"
+
+typedef jboolean (*JSourceFunc) (jpointer user_data);
+
+typedef struct _JSourceCallbackFuncs JSourceCallbackFuncs;
+typedef struct _JSourceFuncs JSourceFuncs;
+typedef struct _JSource JSource;
+typedef struct _JMainContext JMainContext;
+
+
+#endif
