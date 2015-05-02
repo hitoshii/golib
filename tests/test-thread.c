43d482e3618c218554c1aca7f1095f41347358af
wiiiky <wiiiky@outlook.com>
2015-05-02 16:26:30 +0800

    测试 JWakeup

--- tests/test-thread.c
+++ tests/test-thread.c
@@ -1,22 +1,35 @@
 #include <jlib/jlib.h>
+#include <jlib/jwakeup.h>
 #include <stdio.h>
+#include <unistd.h>
 
 J_PRIVATE_DEFINE_STATIC(private, NULL);
 
 static jpointer thread_func1(jpointer data)
 {
-    printf("%s\n", (const jchar *) data);
+    JWakeup *wakeup = (JWakeup *) data;
+    sleep(1);
+    j_wakeup_signal(wakeup);
     return "hello";
 }
 
 static jpointer thread_func(jpointer data)
 {
+    JWakeup *wakeup = j_wakeup_new();
     JThread *thread = j_thread_new("test-thread", thread_func1,
-                                   "My name is Han Meimei");
+                                   wakeup);
+    JEPoll *ep = j_epoll_new();
+    j_epoll_ctl(ep, j_wakeup_get_pollfd(wakeup), J_EPOLL_CTL_ADD,
+                J_EPOLL_IN, NULL, NULL);
+    JEPollEvent event[1];
+    jint n = j_epoll_wait(ep, event, 1, -1);
     jpointer retval = j_thread_join(thread);
-    if (j_strcmp0(retval, "hello")) {
+    if (n != 1 || j_strcmp0((const jchar *) retval, "hello")) {
         return NULL;
     }
+    j_thread_unref(thread);
+    j_epoll_close(ep);
+    j_wakeup_free(wakeup);
     printf("%s\n", (const jchar *) data);
     return "hello world";
 }
