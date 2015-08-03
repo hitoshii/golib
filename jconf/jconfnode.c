4560c43d2d7a99f4500dd3c519c748552af981b0
wiiiky <wiiiky@outlook.com>
2015-08-03 13:49:14 +0800

    确定JConfNode的功能

--- jconf/jconfnode.c
+++ jconf/jconfnode.c
@@ -28,18 +28,17 @@
 
 /**
  * JConfNodeType:
- * @JCONF_NODE_TYPE_NULL: NULL
- * @JCONF_NODE_TYPE_STRING: jchar*
- * @JCONF_NODE_TYPE_INTEGER: jint64
- * @JCONF_NODE_TYPE_FLOAT: jdouble
- * @JCONF_NODE_TYPE_BOOL: jboolean
- * @JCONF_NODE_TYPE_OBJECT: JConfNode*
- * @JCONF_NODE_TYPE_ARRAY: JPtrArray<JConfNode*>
+ * @J_CONF_NODE_TYPE_NULL: NULL
+ * @J_CONF_NODE_TYPE_STRING: jchar*
+ * @J_CONF_NODE_TYPE_INTEGER: jint64
+ * @J_CONF_NODE_TYPE_FLOAT: jdouble
+ * @J_CONF_NODE_TYPE_BOOL: jboolean
+ * @J_CONF_NODE_TYPE_OBJECT: JHashTable<jchar*, JConfNode*>*
+ * @J_CONF_NODE_TYPE_ARRAY: JPtrArray<JConfNode*>
  */
 
 struct _JConfNode {
     JObject parent;
-    jchar *name;
     JConfNodeType type;
     union {
         jchar *data_string;
@@ -55,86 +54,14 @@ struct _JConfNode {
 #define d_object data.data_object
 #define d_array data.data_array
 
-#define JCONF_NODE_TYPE(n)  ((n)->type)
-#define JCONF_NODE_IS_ARRAY(n)  (JCONF_NODE_TYPE(n)==JCONF_NODE_TYPE_ARRAY)
-#define JCONF_NODE_IS_OBJECT(n) (JCONF_NODE_TYPE(n)==JCONF_NODE_TYPE_OBJECT)
-#define JCONF_NODE_IS_INTEGER(n) (JCONF_NODE_TYPE(n)==JCONF_NODE_TYPE_INTEGER)
-#define JCONF_NODE_IS_STRING(n) (JCONF_NODE_TYPE(n)==JCONF_NODE_TYPE_STRING)
-#define JCONF_NODE_IS_NULL(n) (JCONF_NODE_TYPE(n)==JCONF_NODE_TYPE_NULL)
-#define JCONF_NODE_IS_BOOL(n)   (JCONF_NODE_TYPE(n)==JCONF_NODE_TYPE_BOOL)
-#define JCONF_NODE_IS_FLOAT(n) (JCONF_NODE_TYPE(n)==JCONF_NODE_TYPE_FLOAT)
-
-
-/**
- * j_conf_node_is_array:
- * @node JConfNode
- *
- * Checks to see if node presents a array value
- *
- * Returns TRUE if node has type of JCONF_NODE_TYPE_ARRAY, otherwise FALSE.
- */
-jboolean j_conf_node_is_array(JConfNode * node)
-{
-    return JCONF_NODE_IS_ARRAY(node);
-}
-
-/**
- * j_conf_array_node_get_length:
- * @node: JConfArrayNode
- *
- * Returns: the length of array or zero
- */
-juint j_conf_array_node_get_length(JConfArrayNode * node)
-{
-    if (J_UNLIKELY(!JCONF_NODE_IS_ARRAY(node))) {
-        return 0;
-    }
-    return j_ptr_array_get_len(node->d_array);
-}
-
-/**
- * j_conf_array_node_get:
- * @node: JConfArrayNode
- * @index: the index of node to extract
- *
- * Returns: a JConfNode or %NULL if not found
- */
-JConfNode *j_conf_array_node_get(JConfArrayNode * node, juint index)
-{
-    if (J_UNLIKELY(!JCONF_NODE_IS_ARRAY(node)
-                   || index >= j_ptr_array_get_len(node->d_array))) {
-        return NULL;
-    }
-    return j_ptr_array_get(node->d_array, index);
-}
-
-/**
- * j_conf_node_is_integer:
- * @node: JConfNode
- * 
- * Checks to see if node presents an integer value
- *
- * Returns: TRUE or FALSE
- */
-jboolean j_conf_node_is_integer(JConfNode * node)
-{
-    return JCONF_NODE_IS_INTEGER(node);
-}
-
-/**
- * j_conf_integer_node_get:
- * @node: JConfIntegerNode
- *
- * Returns: the integer value of ndoe
- */
-jint64 j_conf_integer_node_get(JConfIntegerNode * node)
-{
-    if (J_UNLIKELY(!JCONF_NODE_IS_INTEGER(node))) {
-        return -1;
-    }
-    return node->d_integer;
-}
-
+#define J_CONF_NODE_TYPE(n)  ((n)->type)
+#define J_CONF_NODE_IS_ARRAY(n)  (J_CONF_NODE_TYPE(n)==J_CONF_NODE_TYPE_ARRAY)
+#define J_CONF_NODE_IS_OBJECT(n) (J_CONF_NODE_TYPE(n)==J_CONF_NODE_TYPE_OBJECT)
+#define J_CONF_NODE_IS_INTEGER(n) (J_CONF_NODE_TYPE(n)==J_CONF_NODE_TYPE_INTEGER)
+#define J_CONF_NODE_IS_STRING(n) (J_CONF_NODE_TYPE(n)==J_CONF_NODE_TYPE_STRING)
+#define J_CONF_NODE_IS_NULL(n) (J_CONF_NODE_TYPE(n)==J_CONF_NODE_TYPE_NULL)
+#define J_CONF_NODE_IS_BOOL(n)   (J_CONF_NODE_TYPE(n)==J_CONF_NODE_TYPE_BOOL)
+#define J_CONF_NODE_IS_FLOAT(n) (J_CONF_NODE_TYPE(n)==J_CONF_NODE_TYPE_FLOAT)
 /**
  * j_conf_node_get_type:
  * @node:
@@ -146,104 +73,337 @@ JConfNodeType j_conf_node_get_type(JConfNode * node)
     return node->type;
 }
 
-/**
- * j_conf_node_get_name:
- * @node:
- *
- * name of a JConfNode, also known as key
- *
- * Returns: the name of JConfNode
- */
-const jchar *j_conf_node_get_name(JConfNode * node)
-{
-    return node->name;
-}
-
-static inline JConfNode *j_conf_node_alloc(const jchar * name,
-                                           JConfNodeType type, ...);
 static void j_conf_node_free(JConfNode * node);
-static inline JPtrArray *j_conf_node_ptr_array_new(void);
-static inline JHashTable *j_conf_node_object_table_new(void);
 
-/*
- * 创建一个结点
- * 第三个参数根据type类型而定
+/**
+ * j_conf_node_new:
+ * @type: type of node
+ * @...: depends on @type
+ *
+ * Creates a new JConfNode with type.
+ * For string, integer, float, bool, a value must be specified to initialize the node
+ * For object, array, a emptry object or array is created
+ *
+ * Returns: a new JConfNode. free with j_conf_node_unref()
  */
-static inline JConfNode *j_conf_node_alloc(const jchar * name,
-                                           JConfNodeType type, ...)
+JConfNode *j_conf_node_new(JConfNodeType type, ...)
 {
     JConfNode *node = (JConfNode *) j_malloc(sizeof(JConfNode));
     J_OBJECT_INIT(node, j_conf_node_free);
     va_list ap;
     va_start(ap, type);
 
-    node->name = j_strdup(name);
     node->type = type;
     switch (type) {
-    case JCONF_NODE_TYPE_STRING:
+    case J_CONF_NODE_TYPE_STRING:
         node->d_string = j_strdup(va_arg(ap, jchar *));
         break;
-    case JCONF_NODE_TYPE_INTEGER:
+    case J_CONF_NODE_TYPE_INTEGER:
         node->d_integer = va_arg(ap, jint64);
         break;
-    case JCONF_NODE_TYPE_BOOL:
+    case J_CONF_NODE_TYPE_BOOL:
         node->d_integer = va_arg(ap, jint) != 0;
         break;
-    case JCONF_NODE_TYPE_FLOAT:
+    case J_CONF_NODE_TYPE_FLOAT:
         node->d_floating = va_arg(ap, jdouble);
         break;
-    case JCONF_NODE_TYPE_OBJECT:
-        node->d_object = va_arg(ap, JHashTable *);
+    case J_CONF_NODE_TYPE_OBJECT:
+        node->d_object =
+            j_hash_table_new(6, j_str_hash, j_str_equal,
+                             (JKeyDestroyFunc) j_free,
+                             (JValueDestroyFunc) j_object_unref);
         break;
-    case JCONF_NODE_TYPE_ARRAY:
-        node->d_array = va_arg(ap, JPtrArray *);
+    case J_CONF_NODE_TYPE_ARRAY:
+        node->d_array =
+            j_ptr_array_new_full(0, (JDestroyNotify) j_object_unref);
         break;
     default:
-        node->type = JCONF_NODE_TYPE_NULL;
+        node->type = J_CONF_NODE_TYPE_NULL;
         break;
     }
     return node;
 }
 
 /*
- * 创建结点数组
- */
-static inline JPtrArray *j_conf_node_ptr_array_new(void)
-{
-    JPtrArray *array =
-        j_ptr_array_new_full(0, (JDestroyNotify) j_object_unref);
-    return array;
-}
-
-/*
- * 创建对象存储表
- */
-static inline JHashTable *j_conf_node_object_table_new(void)
-{
-    JHashTable *table = j_hash_table_new(5, j_str_hash, j_str_equal, NULL,
-                                         (JValueDestroyFunc)
-                                         j_object_unref);
-    return table;
-}
-
-
-/*
  * 销毁一个结点
  */
 static void j_conf_node_free(JConfNode * node)
 {
-    j_free(node->name);
     switch (node->type) {
-    case JCONF_NODE_TYPE_STRING:
+    case J_CONF_NODE_TYPE_STRING:
         j_free(node->d_string);
         break;
-    case JCONF_NODE_TYPE_OBJECT:
+    case J_CONF_NODE_TYPE_OBJECT:
         j_hash_table_free_full(node->d_object);
         break;
-    case JCONF_NODE_TYPE_ARRAY:
+    case J_CONF_NODE_TYPE_ARRAY:
         j_ptr_array_free(node->d_array, TRUE);
         break;
     default:
         break;
     }
+}
+
+/**
+ * j_conf_node_is_null:
+ * @node: JConfNode
+ *
+ * Checks to see if node has type of J_CONF_NODE_TYPE_NULL
+ *
+ * Returns: TRUE otherwise FALSE
+ */
+jboolean j_conf_node_is_null(JConfNode * node)
+{
+    return J_CONF_NODE_IS_NULL(node);
+}
+
+/**
+ * j_conf_node_is_integer:
+ * @node: JConfNode
+ *
+ * Checks to see if node has type of J_CONF_NODE_TYPE_INTEGER
+ *
+ * Returns: TRUE otherwise FALSE
+ */
+jboolean j_conf_node_is_integer(JConfNode * node)
+{
+    return J_CONF_NODE_IS_INTEGER(node);
+}
+
+/**
+ * j_conf_node_is_string:
+ * @node: JConfNode
+ *
+ * Checks to see if node has type of J_CONF_NODE_TYPE_INTEGER
+ *
+ * Returns: TRUE otherwise FALSE
+ */
+jboolean j_conf_node_is_string(JConfNode * node)
+{
+    return J_CONF_NODE_IS_STRING(node);
+}
+
+/**
+ * j_conf_node_is_float:
+ * @node: JConfNode
+ *
+ * Checks to see if node has type of J_CONF_NODE_TYPE_FLOAT
+ *
+ * Returns: TRUE otherwise FALSE
+ */
+jboolean j_conf_node_is_float(JConfNode * node)
+{
+    return J_CONF_NODE_IS_FLOAT(node);
+}
+
+/**
+ * j_conf_node_is_bool:
+ * @node: JConfNode
+ *
+ * Checks to see if node has type of J_CONF_NODE_TYPE_BOOL
+ *
+ * Returns: TRUE otherwise FALSE
+ */
+jboolean j_conf_node_is_bool(JConfNode * node)
+{
+    return J_CONF_NODE_IS_BOOL(node);
+}
+
+/**
+ * j_conf_node_is_object:
+ * @node: JConfNode
+ *
+ * Checks to see if node has type of J_CONF_NODE_TYPE_OBJECT
+ *
+ * Returns: TRUE otherwise FALSE
+ */
+jboolean j_conf_node_is_object(JConfNode * node)
+{
+    return J_CONF_NODE_IS_OBJECT(node);
+}
+
+/**
+ * j_conf_node_is_array:
+ * @node: JConfNode
+ *
+ * Checks to see if node has type of J_CONF_NODE_TYPE_ARRAY
+ *
+ * Returns: TRUE otherwise FALSE
+ */
+jboolean j_conf_node_is_array(JConfNode * node)
+{
+    return J_CONF_NODE_IS_ARRAY(node);
+}
+
+/**
+ * j_conf_integer_get:
+ * @node: JConfInteger
+ *
+ * Returns: integer value
+ */
+jint64 j_conf_integer_get(JConfInteger * node)
+{
+    if (J_UNLIKELY(!J_CONF_NODE_IS_INTEGER(node))) {
+        return -1;
+    }
+    return node->d_integer;
+}
+
+/**
+ * j_conf_integer_set:
+ * @node: JConfInteger
+ * @integer: the new integer value
+ */
+void j_conf_integer_set(JConfInteger * node, jint64 integer)
+{
+    if (J_UNLIKELY(!J_CONF_NODE_IS_INTEGER(node))) {
+        return;
+    }
+    node->d_integer = integer;
+}
+
+/**
+ * j_conf_string_get:
+ * @node: JConfString
+ *
+ * Returns: string value
+ */
+const jchar *j_conf_string_get(JConfString * node)
+{
+    if (J_UNLIKELY(!J_CONF_NODE_IS_STRING(node))) {
+        return NULL;
+    }
+    return node->d_string;
+}
+
+/**
+ * j_conf_float_get:
+ * @node: JConfFloat
+ *
+ * Returns: float value
+ */
+jdouble j_conf_float_get(JConfFloat * node)
+{
+    if (J_UNLIKELY(!J_CONF_NODE_IS_FLOAT(node))) {
+        return 0;
+    }
+    return node->d_floating;
+}
+
+/**
+ * j_conf_bool_get:
+ * @node: JConfBool
+ *
+ * Returns: boolean value
+ */
+jboolean j_conf_bool_get(JConfBool * node)
+{
+    if (J_UNLIKELY(!J_CONF_NODE_IS_BOOL(node))) {
+        return FALSE;
+    }
+    return (jboolean) node->d_integer;
+}
+
+/**
+ * j_conf_array_get_length:
+ * @node: JConfArray
+ *
+ * Returns: length of array
+ */
+juint j_conf_array_get_length(JConfArray * node)
+{
+    if (J_UNLIKELY(!J_CONF_NODE_IS_ARRAY(node))) {
+        return 0;
+    }
+    return j_ptr_array_get_len(node->d_array);
+}
+
+/**
+ * j_conf_array_get:
+ * @node: JConfArray
+ * @index: index of node
+ *
+ * Returns: the JConfNode at index
+ */
+JConfNode *j_conf_array_get(JConfArray * node, juint index)
+{
+    if (J_UNLIKELY(!J_CONF_NODE_IS_ARRAY(node))) {
+        return NULL;
+    }
+    return j_ptr_array_get(node->d_array, index);
+}
+
+/**
+ * j_conf_array_append:
+ * @array: JConfArray
+ * @node: the node to append
+ *
+ * if array is not a valid JConfArray, nothing will be changed
+ */
+void j_conf_array_append(JConfArray * array, JConfNode * node)
+{
+    if (J_UNLIKELY(!J_CONF_NODE_IS_ARRAY(array))) {
+        return;
+    }
+    j_ptr_array_append_ptr(array->d_array, node);
+}
+
+/**
+ * j_conf_array_append_integer:
+ * @array: JConfArray
+ * @integer: the integer value to append
+ */
+void j_conf_array_append_integer(JConfArray * array, jint64 integer)
+{
+    j_conf_array_append(array,
+                        j_conf_node_new(J_CONF_NODE_TYPE_INTEGER,
+                                        integer));
+}
+
+/**
+ * j_conf_array_append_string:
+ * @array: JConfArray
+ * @string: the string value to append
+ */
+void j_conf_array_append_string(JConfArray * array, const jchar * string)
+{
+    j_conf_array_append(array,
+                        j_conf_node_new(J_CONF_NODE_TYPE_STRING, string));
+}
+
+
+/**
+ * j_conf_array_append_bool:
+ * @array: JConfArray
+ * @b: the boolean value to append
+ */
+void j_conf_array_append_bool(JConfArray * array, jboolean b)
+{
+    j_conf_array_append(array, j_conf_node_new(J_CONF_NODE_TYPE_BOOL, b));
+}
+
+/**
+ * j_conf_array_append_float:
+ * @array: JConfArray
+ * @floating: the float value to append
+ */
+void j_conf_array_append_float(JConfArray * array, jdouble floating)
+{
+    j_conf_array_append(array,
+                        j_conf_node_new(J_CONF_NODE_TYPE_FLOAT, floating));
+}
+
+/**
+ * j_conf_object_get:
+ * @node: JConfObject
+ * @name: the key of child node
+ *
+ * Returns: JConfNode or NULL if not found
+ */
+JConfNode *j_conf_object_get(JConfObject * node, const jchar * name)
+{
+    if (J_UNLIKELY(!J_CONF_NODE_IS_OBJECT(node))) {
+        return NULL;
+    }
+    return (JConfNode *) j_hash_table_find(node->d_object, name);
 }
--- jconf/jconfnode.h
+++ jconf/jconfnode.h
@@ -22,36 +22,58 @@
 
 /**
  * JConfNode:
- * 
+ *
  * The `JConfNode` struct is opaque data type
  * presenting a kind of data
  */
 typedef struct _JConfNode JConfNode;
+typedef struct _JConfNode JConfObject;
+typedef struct _JConfNode JConfNull;
+typedef struct _JConfNode JConfInteger;
+typedef struct _JConfNode JConfString;
+typedef struct _JConfNode JConfArray;
+typedef struct _JConfNode JConfBool;
+typedef struct _JConfNode JConfFloat;
 
 typedef enum {
-    JCONF_NODE_TYPE_NULL = 0,   /* 空值 */
-    JCONF_NODE_TYPE_STRING,     /* 字符串 */
-    JCONF_NODE_TYPE_INTEGER,    /* 整数 */
-    JCONF_NODE_TYPE_FLOAT,      /* 浮点数 */
-    JCONF_NODE_TYPE_BOOL,       /* 逻辑值 */
-    JCONF_NODE_TYPE_OBJECT,     /* 对象 */
-    JCONF_NODE_TYPE_ARRAY       /* 数组 */
+    J_CONF_NODE_TYPE_NULL = 0,  /* 空值 */
+    J_CONF_NODE_TYPE_STRING,    /* 字符串 */
+    J_CONF_NODE_TYPE_INTEGER,   /* 整数 */
+    J_CONF_NODE_TYPE_FLOAT,     /* 浮点数 */
+    J_CONF_NODE_TYPE_BOOL,      /* 逻辑值 */
+    J_CONF_NODE_TYPE_OBJECT,    /* 对象 */
+    J_CONF_NODE_TYPE_ARRAY      /* 数组 */
 } JConfNodeType;
-
 
 #define j_conf_node_ref(n)  J_OBJECT_REF(n)
 #define j_conf_node_unref(n) J_OBJECT_UNREF(n)
 
+JConfNode *j_conf_node_new(JConfNodeType type, ...);
+
 JConfNodeType j_conf_node_get_type(JConfNode * node);
-const jchar *j_conf_node_get_name(JConfNode * node);
 
-typedef JConfNode JConfIntegerNode;
+jboolean j_conf_node_is_null(JConfNode * node);
 jboolean j_conf_node_is_integer(JConfNode * node);
-jint64 j_conf_integer_node_get(JConfIntegerNode * node);
+jboolean j_conf_node_is_string(JConfNode * node);
+jboolean j_conf_node_is_float(JConfNode * node);
+jboolean j_conf_node_is_bool(JConfNode * node);
+jboolean j_conf_node_is_object(JConfNode * node);
+jboolean j_conf_node_is_array(JConfNode * array);
 
-typedef JConfNode JConfArrayNode;
-jboolean j_conf_node_is_array(JConfNode * node);
-juint j_conf_array_node_get_length(JConfArrayNode * node);
-JConfNode *j_conf_array_node_get(JConfArrayNode * node, juint index);
+jint64 j_conf_integer_get(JConfInteger * node);
+void j_conf_integer_set(JConfInteger * node, jint64 integer);
+const jchar *j_conf_string_get(JConfString * node);
+jdouble j_conf_float_get(JConfFloat * node);
+jboolean j_conf_bool_get(JConfBool * node);
+
+juint j_conf_array_get_length(JConfArray * node);
+JConfNode *j_conf_array_get(JConfArray * node, juint index);
+void j_conf_array_append(JConfArray * array, JConfNode * node);
+void j_conf_array_append_integer(JConfArray * array, jint64 integer);
+void j_conf_array_append_string(JConfArray * array, const jchar * string);
+void j_conf_array_append_bool(JConfArray * array, jboolean b);
+void j_conf_array_append_float(JConfArray * array, jdouble floating);
+
+JConfNode *j_conf_object_get(JConfObject * node, const jchar * name);
 
 #endif
